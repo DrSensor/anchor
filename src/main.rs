5e67a50a834842f217ab4cb13b66c46321c5d882
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2020-02-24 05:19:09 +0700

    inspect all event in pulldown-cmark

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use pulldown_cmark::Parser;
+use std::io::{Read, stdin};
+
 fn main() {
-    println!("Hello, world!");
+    // let args = env::args().collect();
+
+    let mut buffer = String::new();
+    stdin().read_to_string(&mut buffer).expect("EOF");
+
+    let parser = Parser::new(&buffer);
+    for event in parser {
+        println!("{:?}", event);
+    }
 }
